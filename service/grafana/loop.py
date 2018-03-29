f03108296783201c89ac5a1f1487e15f94532048
StacyChen <stacychen@email.com>
2018-03-29 15:49:15 +0800

    update grafana python

--- service/grafana/loop.py
+++ service/grafana/loop.py
@@ -8,17 +8,34 @@ from common import *
 
 
 def main():
-    grafanaHead = "engineering.kli.v3.tracking"
+    # grafanaHead = "engineering.kli.v3.tracking"
+    # targets = ["ticket.goal", "ticket.actual", "meet.plan", "dontmeet.plan","spend.times"]
+    grafanaHead = "kv.tracking"
     env = raw_input("ENV? ( production / staging / dev ) ")
     domain = "{}.{}.".format(grafanaHead, str(env))
-    targets = ["ticket.goal", "ticket.actual", "meet.plan", "dontmeet.plan","spend.times"]
-    # targets = ["spend.times"]
+    targets = [
+        "max.spending.times",
+        "total.clear.times",
+        "total.get.times",
+        "total.info.times",
+        "total.delete.times",
+        "total.set.times",
+        "total.get.times",
+        "delete.avg.times",
+        "delete.goal.times"
+    ]
     start = 0
     end = input("Count? ")
     value_start = input("Value Range(?~)? ")
     value_end = input("Value Range(~?)? ")
-    timestamp = int(time.time()) - 3600
-    interval = int(3600 / (end + 1))
+
+    # calculate timestamp
+    now = int(time.time())
+    def_timestamp = now - 3600
+    timestamp = int(raw_input("timestamp : ") or str(def_timestamp))
+    interval = abs(now - timestamp)
+    interval = int(interval / (end + 1))
+
     for x in xrange(start, end):
         timestamp = timestamp + interval
         print "-------------------"
@@ -26,10 +43,9 @@ def main():
         print "-------------------"
         for target in targets:
             value = random.randint(value_start, value_end)
-            common().graphiteLog(domain + target, value,timestamp)
+            common().graphiteLog(domain + target, value, timestamp)
             print "    Send To \"{}\" Data: {} ".format(domain + target, value)
         # time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
-
